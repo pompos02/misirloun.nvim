d19c1269f2657228eb942bcc3043829eedebad8b
pompos02 <yiannis.caravellas@gmail.com>
2025-10-14 16:03:58 +0300

    exp blink

--- lua/misirlou.lua
+++ lua/misirlou.lua
@@ -1018,7 +1018,7 @@ local function set_highlights()
         BlinkCmpScrollBarGutter = { bg = palette.bg },
         BlinkCmpLabel = { fg = palette.text, bg = palette.bg },
         BlinkCmpLabelDeprecated = { fg = palette.muted, bg = palette.bg },
-        BlinkCmpLabelMatch = { fg = palette.string, bg = palette.bg },
+        BlinkCmpLabelMatch = { bg = palette.text, fg = palette.bg, bold = styles.bold, underline = true },
         BlinkCmpLabelDetail = { fg = palette.subtle, bg = palette.bg },
         BlinkCmpLabelDescription = { fg = palette.subtle, bg = palette.bg },
         BlinkCmpKind = { fg = palette.purple, bg = palette.bg },
