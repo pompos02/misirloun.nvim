442957832ba6e814f054db465efe62777ed1c4f7
pompos02 <yiannis.caravellas@gmail.com>
2025-10-18 10:54:45 +0300

    good

--- lua/misirloun/palette.lua
+++ lua/misirloun/palette.lua
@@ -13,8 +13,9 @@ local palette = {
     red = "#ce8787",
     gold = "#d4ab5f",
     green = "#789978",
+    -- string = "#7788AA",
     string = "#93abb3",
-    keys = " #a2a2c3 ",
+    keys = " #a2a2c3",
     white = "#ffffff",
 }
 
